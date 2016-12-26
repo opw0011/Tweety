b6f51ad5e4e28a082c61f40a6338b87d19056817
BennyO <opw0011@gmail.com>
2016-12-26 22:41:51 +0800

    Add simple send tweet button and Twitter view controller

--- Tweety/ViewController.swift
+++ Tweety/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func SendTweetButtonClicked(_ sender: UIButton) {
+        NSLog("Send tweet button is clicked")
+        
+        // check whether twitter is available in the device
+        if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter) {
+            
+            // create a tweeter view controller
+            let tweetVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            
+            tweetVC?.setInitialText("Testing String")
+            
+            // show the tweet view controller
+            self.present(tweetVC!, animated: true, completion: nil)
+        } else {
+            NSLog("Your device does not support twitter")
+        }
+        
+    }
 
 }
 
